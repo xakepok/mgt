7479f108361bfcf1ea88db8fd554b1598a81d731
asharikov <xakepok@xakepok.com>
2018-01-15 19:25:09 +0300

    Выводим версию компонента

--- components/views/mgt/tmpl/default.php
+++ components/views/mgt/tmpl/default.php
@@ -10,5 +10,7 @@ HTMLHelper::_('stylesheet', 'com_mgt/style.css', array('version' => 'auto', 'rel
     <div><?php echo $this->loadTemplate('stat');?></div>
     <br>
     <div><?php echo $this->loadTemplate('filter');?></div>
+    <br>
+    <div><?php echo $this->loadTemplate('version');?></div>
 </div>
 <div class="clear"></div>
